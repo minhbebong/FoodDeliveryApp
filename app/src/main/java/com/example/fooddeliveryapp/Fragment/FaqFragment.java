87a30629db70368a577844eff4ca97a2c581ebe1
Lại Thế Đạt <104337230+LaiTheDat@users.noreply.github.com>
2023-07-17 22:55:15 +0700

    Setting Screen (#7)
    * update setting screen
    
    * update setting screen
    
    * update UI setting screen
    
    * update
    
    * Update fragment_setting.xml
    
    ---------
    
    Co-authored-by: Nguyen Thanh Dat <datntcx330@gmail.com>

---
+++ app/src/main/java/com/example/fooddeliveryapp/Fragment/FaqFragment.java
@@ -0,0 +1,23 @@
+package com.example.fooddeliveryapp.Fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fooddeliveryapp.R;
+
+public class FaqFragment extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_faq);
+
+
+
+    }
+}
